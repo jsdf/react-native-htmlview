1424be8139b2f93c93074f195da8772aac5ec55a
James Friend <james@jsdf.co>
2016-04-10 09:36:31 +1000

    fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,4 @@
-// module.exports = {
-  // extends: "eslint:recommended",
-//   parser: "babel-eslint",
-//   parserOptions: {
-//     ecmaVersion: 6,
-//     sourceType: "module",
-//   },
-//   env: {
-//     node: true,
-//     es6: true,
-//   },
-//   plugins: [
-//     "eslint-plugin-react",
-//   ],
-//   rules: {
-//     "no-console": 0,
-//     "no-shadow": 2,
-//     // style
-//     semi: ["error", "never"],
-//     "no-extra-semi": 2,
-//     "no-multi-spaces": 2,
-//     "array-bracket-spacing": 2,
-//     "block-spacing": 2,
-//     "comma-spacing": 2,
-//     "comma-dangle": [2, "always-multiline"],
-//     "computed-property-spacing": 2,
-//     "eol-last": 2,
-//     "indent": [2, 2],
-//     "keyword-spacing": 2,
-//     "linebreak-style": 2,
-//     "no-spaced-func": 2,
-//     "no-trailing-spaces": 2,
-//     "object-curly-spacing": 2,
-//     "quotes": [2, "single", "avoid-escape"],
-//     "semi": 2,
-//     "semi-spacing": 2,
-//     "space-before-blocks": 2,
-//     "space-before-function-paren": [2, "never"],
-//     "space-in-parens": 2,
-//     "space-infix-ops": 2,
-//     "space-unary-ops": 2,
-//     "arrow-spacing": 2,
-//     "generator-star-spacing": 2,
-//     "template-curly-spacing": 2,
-//     "yield-star-spacing": 2,
-//   }
-// };
+
 module.exports = {
   "extends": ["eslint:recommended", "plugin:react/recommended"],
   env: {
@@ -68,5 +22,29 @@ module.exports = {
     // style
     "semi": [2, 'never'],
     "comma-dangle": [2, "always-multiline"],
+    "no-extra-semi": 2,
+    "no-multi-spaces": 2,
+    "array-bracket-spacing": 2,
+    "block-spacing": 2,
+    "comma-spacing": 2,
+    "computed-property-spacing": 2,
+    "eol-last": 2,
+    "indent": [2, 2],
+    "keyword-spacing": 2,
+    "linebreak-style": 2,
+    "no-spaced-func": 2,
+    "no-trailing-spaces": 2,
+    "object-curly-spacing": 2,
+    "quotes": [2, "single", "avoid-escape"],
+    "semi-spacing": 2,
+    "space-before-blocks": 2,
+    "space-before-function-paren": [2, "never"],
+    "space-in-parens": 2,
+    "space-infix-ops": 2,
+    "space-unary-ops": 2,
+    "arrow-spacing": 2,
+    "generator-star-spacing": 2,
+    "template-curly-spacing": 2,
+    "yield-star-spacing": 2,
   }
 };
--- HTMLView.js
+++ HTMLView.js
@@ -41,7 +41,7 @@ function htmlToElement(rawHtml, opts, done) {
             {node.name == 'li' ? BULLET : null}
             {domToElement(node.children, node)}
             {node.name == 'br' || node.name == 'li' ? LINE_BREAK : null}
-            {node.name == 'p' && index < list.length-1 ? PARAGRAPH_BREAK : null}
+            {node.name == 'p' && index < list.length - 1 ? PARAGRAPH_BREAK : null}
             {node.name == 'h1' || node.name == 'h2' || node.name == 'h3' || node.name == 'h4' || node.name == 'h5' ? PARAGRAPH_BREAK : null}
           </Text>
         )
