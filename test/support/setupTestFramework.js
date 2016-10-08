859698329884a38e9817c52bc33f5f625bf40287
Ken Gorab <ken.gorab@gmail.com>
2016-10-07 22:50:26 -0400

    When creating the global jsdom, make sure all window variables are on global as well (like navigator, for example, which ReactDOM assumes exists)

--- test/support/setupTestFramework.js
+++ test/support/setupTestFramework.js
@@ -9,6 +9,17 @@ const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
 global.document = doc
 global.window = doc.defaultView
 
+function propagateToGlobal(window) {
+  for (let key in window) {
+    if (!window.hasOwnProperty(key)) continue
+    if (key in global) continue
+
+    global[key] = window[key]
+  }
+}
+
+propagateToGlobal(global.window)
+
 const chai = require('chai')
 
 global.expect = chai.expect
