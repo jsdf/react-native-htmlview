e0734ca791f53ba8372d86d1c4c65f7054b3d353
Ken Gorab <ken.gorab@gmail.com>
2016-10-07 22:34:54 -0400

    Stabilizes unit tests   - Fixes #54   - Replaces jest with mocha, since I was having issues with jest's auto-mocking     - I feel that mocha is becoming the standard js unit test runner anyway   - Uses enzyme for shallow/deep rendering instead of react-addons-testutils

--- test/support/setupTestFramework.js
+++ test/support/setupTestFramework.js
@@ -1,3 +1,14 @@
+require('babel-register')({
+  presets: ['es2015', 'es2016', 'react-native'],
+  ignore: /node_modules\/(?!react-native)/,
+})
+require('react-native-mock/mock')
 
-window.shallowRender = require.requireActual('./shallowRender')
-jest.setMock('react-native', require.requireActual('../../__mocks__/react-native'))
+const jsdom = require('jsdom')
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
+global.document = doc
+global.window = doc.defaultView
+
+const chai = require('chai')
+
+global.expect = chai.expect
