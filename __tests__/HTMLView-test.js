b8b1892a9c89ad659ed5ce98965522db9d522388
alphasp <gmerudotcom@gmail.com>
2017-12-19 18:44:03 +0800

    lint

--- __tests__/HTMLView-test.js
+++ __tests__/HTMLView-test.js
@@ -144,11 +144,7 @@ describe('<HTMLView/>', () => {
 
     function renderNode(node, index) {
       if (node.name == 'thing') {
-        return (
-          <Text key={index}>
-            {node.attribs.b}
-          </Text>
-        );
+        return <Text key={index}>{node.attribs.b}</Text>;
       }
     }
 
